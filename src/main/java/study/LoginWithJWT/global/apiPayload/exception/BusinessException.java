ae99db4fc52a9027d7e59a9c65b05fe9d7b89359
jaemin0413 <95339052+jaemin0413@users.noreply.github.com>
2025-09-10 22:37:21 +0900

    [SETTING] 포괄적 Exception 생성

---
+++ src/main/java/study/LoginWithJWT/global/apiPayload/exception/BusinessException.java
@@ -0,0 +1,42 @@
+package study.LoginWithJWT.global.apiPayload.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import study.LoginWithJWT.global.apiPayload.code.BaseCode;
+import study.LoginWithJWT.global.apiPayload.code.ErrorCode;
+
+@Getter
+public class BusinessException extends RuntimeException {
+
+    private final ErrorCode errorCode;
+
+    public BusinessException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        // RuntimeException의 객체를 먼저 만듦
+        // 이는 RuntimeException이 가지고 있는 세 가지 기능을 상속받기 위함
+        // 1. 위의 생성자를 통해 전달받은 에러 메세지 문자열을 객체 내부에 저장
+        // 2. 코드가 실행되는 순간 부모인 RuntimeException의 생성자가 호출되며 그 시점의 모든 메서드 호출 기록을 객체 내부에 저장
+        // 3, Exception을 직접 상속받는 것보다 깔끔한 코드 구현 가능
+        this.errorCode = errorCode;
+
+    }
+
+    public BaseCode getCode() {
+        return errorCode;
+    }
+
+    public HttpStatus getStatus() {
+        return errorCode.getStatus();
+    }
+
+    public String getErrorCode() {
+        return errorCode.getCode();
+    }
+
+    public String getErrorMessage() {
+        return errorCode.getMessage();
+    }
+
+    // 이렇게 반환받은 값들은 @ExceptionHandler에서 사용함
+
+}
